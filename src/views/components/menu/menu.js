a7613f4f00e6f39183fa35234aba985564ea4712
Kurdant <hugopaynaud4@gmail.com>
2024-10-21 10:15:09 +0200

    menu style changed, media queries projects

--- src/views/components/menu/menu.js
+++ src/views/components/menu/menu.js
@@ -1,12 +1,19 @@
-//old menu pull bottom
 import React, { useState } from "react";
 import './menu.css';
+import { useNavigate } from "react-router-dom";
+import colors from "../../../assets/colors/colors";
 
 function Menu() {
+  const navigate = useNavigate();
   const [isPulldownOpen, setIsPulldownOpen] = useState(false);
 
   const togglePulldown = () => {
     setIsPulldownOpen(!isPulldownOpen);
+  };
+
+  const handleLinkClick = (path) => {
+    navigate(path);
+    setIsPulldownOpen(false); // Ferme le menu après la navigation
   };
 
   return (
@@ -20,12 +27,12 @@ function Menu() {
       </div>
 
       <div className={`pulldown ${isPulldownOpen ? 'active' : ''}`}>
-        <div className="close-btn" onClick={togglePulldown}>✖</div>
-        <div className="menu_link">Home</div>
-        <div className="menu_link">About</div>
-        <div className="menu_link">Projects</div>
-        <div className="menu_link">Contact</div>
-        <div className="menu_link">More</div>
+        <div className="close-btn" onClick={togglePulldown} style={{ color: colors.primary }}>✖</div>
+        <div className="menu_link margin_top_menu" onClick={() => handleLinkClick('/')} style={{ color: colors.primary }}>Home</div>
+        <div className="menu_link" onClick={() => handleLinkClick('/about')} style={{ color: colors.primary }}>About</div>
+        <div className="menu_link" onClick={() => handleLinkClick('/projects')} style={{ color: colors.primary }}>Projects</div>
+        <div className="menu_link" onClick={() => handleLinkClick('/contact')} style={{ color: colors.primary }}>Contact</div>
+        <div className="menu_link" onClick={() => handleLinkClick('/more')} style={{ color: colors.primary }}>More</div>
       </div>
     </div>
   );
